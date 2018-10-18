8b552ecc4ebced2f9e12a7084b64069c0c676eab
developerMHG <developer.golestani@gmail.com>
2018-10-18 16:21:17 +0330

    Update

--- header.php
+++ header.php
@@ -1,10 +1,11 @@
+<?php $ref = rand()*rand(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>SUT</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<link rel="stylesheet" href="assets/css/styles.css">
+	<link rel="stylesheet" href="assets/css/styles.css?<?php echo $ref; ?>">
 </head>
 <body>
 	<div id="header">
