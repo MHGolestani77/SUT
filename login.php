69a449de375487f483f6079e3a76e5abfa28437a
developerMHG <developer.golestani@gmail.com>
2018-10-18 13:50:14 +0330

    update login

--- login.php
+++ login.php
@@ -6,13 +6,10 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="stylesheet" href="assets/css/styles.css">
 </head>
-<body>
+<body class="login">
 
-	<div class="container col-3">
-		<!-- logo image -->
+	<!-- <div class="container">
 		<img src="assets/img/logo.png" class="img-responsive">
-		<!-- end logo -->
-		<!-- login -->
 		<div class="login">
 			<form >
 				<input type="text" name="id" class="form-control" placeholder="نام کاربری">
@@ -20,20 +17,27 @@
 				<button type="submit" class="btn btn-primary btn-lg btn-block">ورود</button>
 			</form>
 		</div>
-
-		
-
-	</div>
-
-<div id="footer" class="bg-dark">
-		<div class="details col-12">
-			<p class="h6 my-2">بسیج دانشجویی دانشگاه صنعتی شاهرود</p>
+	</div> -->
+	<div id="login" class="container">
+		<div class="row">
+			<div class="col-sm-4 col-md-4">
+				<img src="assets/img/logo.png" class="col-10">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4 col-md-4">
+				<div class="login rounded">
+					<form action="">
+						<input type="text" name="id" class="form-control" placeholder="نام کاربری">
+						<input type="password" name="password" class="form-control" placeholder="گذر واژه">
+						<button type="submit" class="btn btn-primary btn-lg btn-block">ورود</button>
+					</form>
+				</div>
+			</div>
 		</div>
 	</div>
 
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
-</body>
-</html>
+<?php 
+	include "footer.php";
+?>
