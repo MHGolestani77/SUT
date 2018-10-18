6a1546c75bc3b51b65679de41d97dfacff4e2faf
developerMHG <developer.golestani@gmail.com>
2018-10-18 09:58:05 +0330

    Update

--- login.php
+++ login.php
@@ -1,7 +1,13 @@
-<?php 
-	include "header.php";
-?>
-	
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>SUT</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+
 
 	<!-- Inja codato benevis -->
 	<!-- <h1></h1> -->
